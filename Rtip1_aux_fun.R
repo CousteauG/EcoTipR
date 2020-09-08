cdeec807ed2e40e0b6dc111ac4a38dabe897f4f7
CousteauG <70930021+CousteauG@users.noreply.github.com>
2020-09-08 12:16:33 -0500

    Rtip # 1
    Crea un archivo GIF mostrando mapas de chl-a del norte del Perú por año.

---
+++ Rtip1_aux_fun.R
@@ -0,0 +1,72 @@
+getURL = function(Var, Lons, Lats, Each = 1, Res = '4km', iDate, timeResolution = 'YR'){
+
+  if(timeResolution == 'YR') {
+    date_1 = as.Date(iDate, "%Y-%m-%d")
+    Yr = format(date_1, format="%Y")
+    iniDate = paste0(Yr, '-01-01')
+    endDate = paste0(Yr, '-12-31')
+    Dates = transDate(begin = iniDate, end = endDate)
+    YearNumber = paste0(Dates$beginDOY, Dates$endDOY)
+  }
+  
+  
+  Year1 = strftime(x = strptime(x = Dates$begin, format = '%Y-%m-%d'), format = '%Y')
+  Number1 = substr(x = Dates$beginDOY, start = 5, stop = 7)
+  
+  if(Res == '4km'){
+    
+    maxIndLat = 4319
+    maxIndLon = 8639
+    
+  }
+  
+  if(Res == '9km'){
+    
+    maxIndLat = 2159
+    maxIndLon = 4319
+    
+  }
+  
+  if(Var == 'sst') Var2 = 'SST'
+  if(Var == 'chlor_a') Var2 = 'CHL'
+  
+  
+  
+  FakeLat = c(-90,90) + 90
+  LatInd2 = maxIndLat - (floor(((Lats[1]+90)*maxIndLat)/max(FakeLat)))
+  LatInd1 = maxIndLat - (ceiling(((Lats[2]+90)*maxIndLat)/max(FakeLat)))
+  
+  FakeLon = c(-180,180) + 180
+  LonInd1 = floor(((Lons[1]+180)*maxIndLon)/max(FakeLon))
+  LonInd2 = ceiling(((Lons[2]+180)*maxIndLon)/max(FakeLon))
+  
+  StrLat = paste0('[', LatInd1, ':', Each, ':', LatInd2, ']')
+  StrLon = paste0('[', LonInd1, ':', Each, ':', LonInd2, ']')
+  
+  StrVar = paste0(StrLat, StrLon)
+  
+  
+  outURL = paste0('https://oceandata.sci.gsfc.nasa.gov:443/opendap/MODISA/L3SMI/',
+                  Year1, '/',
+                  Number1, '/',
+                  'A', YearNumber,
+                  '.L3m_', timeResolution, '_', Var2, '_', Var, '_',
+                  Res, '.nc?', Var, StrVar, ',lat', StrLat, ',lon', StrLon)
+  
+  return(outURL)
+  
+}
+
+
+
+numberOfDays <- function(date) {
+  m <- format(date, format="%m")
+  
+  while (format(date, format="%m") == m) {
+    date <- date + 1
+  }
+  
+  return(as.integer(format(date - 1, format="%d")))
+}
+
+
---
+++ Rtip1_main.R
@@ -0,0 +1,80 @@
+## LOAD LIBRARIES
+library(rmarkdown)
+library(tinytex)
+library(knitr)
+library(ncdf4)
+library(maps)
+library(fields)
+library(pracma)
+library(raster)
+library(MODIS)
+require(splancs)
+source('Rtip1_aux_fun.R')
+
+#SET YOUR WD FIRST!
+
+# Here your data
+myData = expand.grid(YEAR = 2003:2019, MONTH = 1, DAY = 1)
+
+# Paste year, month, and day info:
+myData$time = paste0(myData$YEAR, '-', myData$MONTH, '-', myData$DAY)
+
+# Let's find how many oceanographic files we need to download:
+uniqueDates = unique(myData$time)
+
+
+# Define what variable we want to download and the longitude and latitude ranges:
+ObjVar = 'chlor_a' # only for chl-a
+LongRange = c(-83, -77)
+LatiRange = c(-12, -3)
+
+# Begin loop:
+for(i in seq_along(uniqueDates)){
+  
+  # This function get the URL to download the oceanographic data:
+  # Lons and Lats are the ranges
+  # timeResolution might be 'MO' (monthly) and 'DAY' (daily)
+  url = getURL(Var = ObjVar, Lons = LongRange, Lats = LatiRange, 
+               iDate = uniqueDates[i], timeResolution = 'YR')
+  
+  # Open data:
+  sst.nc<-nc_open(url) # download here
+  
+  # Let's continue:
+  # Get longitude, latitude and time values
+  fillvalue<-ncatt_get(sst.nc,ObjVar,"_FillValue")
+  start.time<-ncatt_get(sst.nc,0,'time_coverage_start')
+  end.time<-ncatt_get(sst.nc,0,'time_coverage_end')
+  lon<-ncvar_get(sst.nc,varid='lon')
+  lat<-ncvar_get(sst.nc,varid='lat')
+  
+  #Extract sst and remove fill in values (set to NA)
+  sst<-ncvar_get(sst.nc,varid=ObjVar)
+  sst[sst==fillvalue$value]<-NA
+  
+  #Close netCDF connection (Important!!)
+  nc_close(sst.nc)
+  
+  png(file=paste0("example_",  myData$YEAR[i], ".png"), 
+      width = 150, height = 140, units = 'mm', res = 400)
+  
+  par(mar = c(4,4,1,4))
+  plot.new()
+  image.plot(lon,sort(lat),log(sst[,order(lat)]), zlim = c(-2, 5), 
+             main=myData$YEAR[i], 
+             ylab='latitude',
+             xlab='longitude', legend.lab = expression(paste("log(mg/m"^3,')')))
+  map("world",fill=T,col="lightgrey",add=T)
+  dev.off()
+  
+  
+}
+
+#Create the animation (GIF):
+system("convert2 -delay 80 *.png example_animation.gif")
+
+# to not leave the directory with the single png files
+# I remove them.
+file.remove(list.files(pattern=".png"))
+
+
